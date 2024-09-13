e577a177813047be85a4748b044974fe498c2528
Nustat1 <poppop399p@gmail.com>
2024-09-13 09:51:13 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7240997878:AAFx0K0xm5ShUCcIQmdsHd9g_wAl-i1o05E')
 
 # Admin user IDs
 admin_id = ["6189146935"]
